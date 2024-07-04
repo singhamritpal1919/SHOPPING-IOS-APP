cc7eea0f47c664625d5582aab16ec84146f9143a
Your Github Username <your-singhamritpal1919@gmail.com>
2024-07-03 23:00:32 -0400

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Amritpal Gill on 2024-07-03.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

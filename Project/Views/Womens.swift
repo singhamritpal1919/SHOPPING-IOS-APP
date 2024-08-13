cb5bb13eaf093286921da50bbc16ba3432349898
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-12 21:30:19 -0400

    modified and new file

---
+++ Project/Views/Womens.swift
@@ -0,0 +1,18 @@
+//
+//  Womens.swift
+//  ProjectApp
+//
+//  Created by Amritpal Gill on 2024-08-12.
+//
+
+import SwiftUI
+
+struct Womens: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Womens()
+}

90b50427c850038ba631889e20f114c6bda2f369
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-13 21:31:27 -0400

    Sign Out View

--- Project/Authentication/SignOutView.swift
+++ Project/Authentication/SignOutView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct SignOutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("You have been signed out")
+            .font(.largeTitle)
+            .padding()
     }
 }
 
-#Preview {
-    SignOutView()
+struct SignOutView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignOutView()
+    }
 }

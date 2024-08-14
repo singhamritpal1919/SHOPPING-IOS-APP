64afacab90ebbe7df4aba06817f7db97f582660a
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-13 21:30:45 -0400

    session manager

--- Project/Authentication/SessionManager.swift
+++ Project/Authentication/SessionManager.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class SessionManager {
+    static let shared = SessionManager()
+
+    private init() {}
+
+    func loginUser(userid: String, completion: @escaping (Bool) -> Void) {
+        // Here you can handle storing the user session, e.g., saving the user ID to UserDefaults
+        UserDefaults.standard.set(userid, forKey: "userID")
+        completion(true)
+    }
+
+    func logoutUser(completion: @escaping (Bool) -> Void) {
+        // Here you can handle removing the user session, e.g., clearing the user ID from UserDefaults
+        UserDefaults.standard.removeObject(forKey: "userID")
+        completion(true)
+    }
+
+    func isLoggedIn() -> Bool {
+        return UserDefaults.standard.string(forKey: "userID") != nil
+    }
+}
+

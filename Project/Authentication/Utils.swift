9a9118a595a4f92c225d9bd55caab52771384001
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-14 23:46:08 -0400

    Auth

--- Project/Authentication/Utils.swift
+++ Project/Authentication/Utils.swift
@@ -5,4 +5,29 @@
 //  Created by Amritpal Gill on 2024-08-08.
 //
 
+
 import Foundation
+
+struct Utils {
+    
+    static func calculateAge(from dateOfBirth: Date) -> Int {
+            let calendar = Calendar.current
+            let currentDate = Date()
+            let ageComponents = calendar.dateComponents([.year], from: dateOfBirth, to: currentDate)
+            return ageComponents.year ?? 0
+        }
+    
+    static func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Z0-9a-z.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+
+    static func isPasswordValid(_ password: String) -> Bool {
+        let passwordRegEx = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+        return passwordTest.evaluate(with: password)
+    }
+}
+
+

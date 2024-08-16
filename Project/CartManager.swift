e0e7389e971a6da7d5e578f409ae474b26fffe79
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-16 17:52:58 -0400

    Cart Manager updated

--- Project/CartManager.swift
+++ Project/CartManager.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+//class CartManager: ObservableObject {
+//    @Published private(set) var products: [Product] = []
+//    @Published private(set) var total: Int = 0
+//    
+//    func addToCart(product: Product) {
+//        products.append(product)
+//        total += product.price
+//    }
+//    
+//    func removeFromCart(product: Product) {
+//        products = products.filter {$0.id != product.id }
+//        total -= product.price
+//    }
+//}
+
+
+class CartManager: ObservableObject {
+    @Published var products: [Product] = []
+    
+    var total: Int {
+        products.reduce(0) { $0 + $1.price }
+    }
+    
+    func addToCart(product: Product) {
+        products.append(product)
+    }
+    
+    func removeFromCart(product: Product) {
+        if let index = products.firstIndex(where: { $0.id == product.id }) {
+            products.remove(at: index)
+        }
+    }
+    
+    
+}

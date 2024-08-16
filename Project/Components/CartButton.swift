5aaee779b32a117473addfc2e646fe7acf6c9979
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-16 17:53:47 -0400

    Cart Button updated

--- Project/Components/CartButton.swift
+++ Project/Components/CartButton.swift
@@ -7,12 +7,48 @@
 
 import SwiftUI
 
-struct CartButton: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+//struct CartButton: View {
+//    var numberOfProducts: Int
+//    
+//    var body: some View {
+//        ZStack(alignment: .topTrailing){
+//            Image(systemName: "cart")
+//                .padding(.top, 5)
+//            
+//            if numberOfProducts > 0 {
+//                Text("\(numberOfProducts)")
+//                    .font(.caption2).bold()
+//                    .foregroundColor(.white)
+//                    .frame(width: 15,height: 15)
+//                    .background(Color(hue: 1.0, saturation: 0.89, brightness: 0.835))
+//                    .cornerRadius(50)
+//            }
+//        }
+//    }
+//}
+//
+//struct CartButton_Preview: PreviewProvider {
+//    static var previews: some View {
+//        CartButton(numberOfProducts: 1)
+//    }
+//}
 
-#Preview {
-    CartButton()
+struct CartButton: View {
+    var numberOfProducts: Int
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing){
+            Image(systemName: "cart")
+                .padding(.top, 5)
+            
+            if numberOfProducts > 0 {
+                Text("\(numberOfProducts)")
+                    .font(.caption2).bold()
+                    .foregroundColor(.white)
+                    .frame(width: 15,height: 15)
+                    .background(Color(hue: 1.0, saturation: 0.89, brightness: 0.835))
+                    .cornerRadius(50)
+            }
+        }
+    }
 }

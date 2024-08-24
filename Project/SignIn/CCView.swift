043f44f839357df555a668b32b3146d9369b158c
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-23 21:39:09 -0400

    CCView updated

--- Project/SignIn/CCView.swift
+++ Project/SignIn/CCView.swift
@@ -8,11 +8,78 @@
 import SwiftUI
 
 struct CCView: View {
+    @StateObject private var databaseManager = DatabaseManager()
+    @State private var items:[Item] = []
+    @State private var newItemName = ""
+    @State private var newItemdescription = ""
+    @State private var isEditeMode = false
+    @State private var selectedItem : Item?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                List{
+                    ForEach(items){ item in
+                        VStack(alignment : .leading){
+                            Text(item.name).font(.headline)
+                            Text(item.description).font(.subheadline)
+                        }
+                        .onTapGesture {
+                            selectedItem = item
+                            newItemName = item.name
+                            newItemdescription = item.description
+                            isEditeMode = true
+                        }
+                        
+                    }
+                    .onDelete(perform : deleteItems)
+                }
+                .onAppear{
+                    databaseManager.fetchItem{ fetchItem in
+                        self.items = fetchItem
+                    
+                    }
+                }
+                
+                HStack{
+                    TextField("Name",text: $newItemName)
+                        .padding()
+                    TextField("Description",text: $newItemdescription)
+                    Button(isEditeMode ? "Update" : "Add"){
+                        if isEditeMode, let item = selectedItem {
+                            var updateditem = item
+                            updateditem.name = newItemName
+                            updateditem.description = newItemdescription
+                            databaseManager.updateItem(updateditem)
+                            isEditeMode = false
+                        }else{
+                            let newItem = Item(name: newItemName, description: newItemdescription)
+                            databaseManager.addItem(newItem)
+                        }
+                        newItemName = ""
+                        newItemdescription = ""
+                        selectedItem = nil
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Items : ")
+            .navigationBarItems(trailing: EditButton())
+        }
+        
+    }
+    
+    private func deleteItems(at offsets : IndexSet){
+        for index in offsets{
+            let item = items[index]
+            databaseManager.deleteItem(item)
+        }
+        items.remove(atOffsets: offsets)
     }
 }
 
-#Preview {
-    CCView()
+struct ContentView_preview : PreviewProvider{
+    static var previews: some View{
+        CCView()
+    }
 }

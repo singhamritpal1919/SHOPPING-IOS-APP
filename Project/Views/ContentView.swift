3a4947e52c66b1d5070e01e265239249cd321050
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-26 01:49:11 -0400

    Content Views

--- Project/Views/ContentView.swift
+++ Project/Views/ContentView.swift
@@ -5,20 +5,78 @@
 //  Created by Amritpal Gill on 2024-07-03.
 //
 
+
+
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                NavigationLink(destination: Mens()) {
+                    CategoryButton(title: "Men", imageName: "men")
+                }
+                .background(.black)
+                .cornerRadius(50)
+                
+                NavigationLink(destination: Womens()) {
+                    CategoryButton(title: "Women", imageName: "women")
+                }
+                .background(.black)
+                .cornerRadius(50)
+                
+                NavigationLink(destination: Kids()) {
+                    CategoryButton(title: "Kids", imageName: "kid1")
+                }
+                .background(.black)
+                .cornerRadius(50)
+                
+                Spacer()
+            }
+            .navigationTitle("Clothing App")
+            .padding()
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct CategoryButton: View {
+    let title: String
+    let imageName: String
+    
+    var body: some View {
+        ZStack {
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 150)
+                .cornerRadius(10)
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .shadow(radius: 10)
+        }
+        .padding(.vertical, 10)
+    }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//
+//
+//            
+//    }
+//}
+
+

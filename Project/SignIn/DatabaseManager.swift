302e4894d24e696e8b3b7c8ecd24706064c0eff5
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-26 01:47:04 -0400

    Database Manager

--- Project/SignIn/DatabaseManager.swift
+++ Project/SignIn/DatabaseManager.swift
@@ -4,5 +4,43 @@
 //
 //  Created by Amritpal Gill on 2024-07-16.
 //
-
 import Foundation
+import FirebaseDatabase
+
+
+class DatabaseManager :ObservableObject{
+    
+    private let database = Database.database().reference();
+    
+    func addItem(_ item: Item){
+        let itemRef = database.child("items").child(item.id)
+        itemRef.setValue(["name": item.name,"description":item.description])
+    }
+    
+    func updateItem(_ item: Item){
+        let itemRef = database.child("items").child(item.id)
+        itemRef.updateChildValues(["name": item.name,"description":item.description])
+    }
+    
+    func deleteItem(_ item: Item){
+        let itemRef = database.child("items").child(item.id)
+        itemRef.removeValue()
+    }
+    
+    func fetchItem(completion: @escaping(([Item]) -> Void )){
+        database.child("items").observe(.value){ snapShot in
+            var items:[Item] = []
+            for child in snapShot.children{
+                if let snapshot = child as? DataSnapshot,
+                   let value = snapshot.value as? [String: Any],
+                   let name = value["name"] as? String,
+                   let description = value["description"] as? String{
+                    let item = Item(id: snapshot.key,name : name,description:description)
+                    items.append(item)
+                }
+            }
+            completion(items)
+        }
+    }
+
+}

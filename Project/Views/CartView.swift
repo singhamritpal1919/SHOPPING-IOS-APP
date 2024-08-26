b5d0e4cf5673148b74e45b82da598092b1b89a7c
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-26 01:48:40 -0400

    Cart Views

--- Project/Views/CartView.swift
+++ Project/Views/CartView.swift
@@ -1,18 +1,103 @@
-//
-//  CartView.swift
-//  Project
-//
-//  Created by Amritpal Gill on 2024-07-04.
+////
+////  CartView.swift
+////  Project
+////
+////  Created by Amritpal Gill on 2024-07-04.
+////
 //
 
 import SwiftUI
 
 struct CartView: View {
+    
+    @State private var showPaymentView: Bool = true
+    @State private var showSignInView: Bool = false
+    @StateObject private var settingViewModel = SettingViewModel() 
+    
+    @EnvironmentObject var cartManager: CartManager
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            NavigationLink(destination: TransactionHistoryView()) {
+                Text("View Transaction History")
+            }
+            .bold()
+            
+            ScrollView {
+                if cartManager.products.count > 0 {
+                    ForEach(cartManager.products, id: \.id) { product in
+                        ProductRow(product: product)
+                    }
+                    
+                    HStack {
+                        Text("Your total cart is")
+                        Spacer()
+                        Text("$\(cartManager.total).00")
+                            .bold()
+                    }
+                    .padding()
+                    
+                    //                PaymentButton(action: {})
+                    //                    .padding()
+                    
+                    
+                    
+                    //                    PaymentButton(action: {
+                    //                        NavigationLink(<#LocalizedStringKey#>, destination: PaymentView()
+                    //                            .environmentObject(cartManager))
+                    //                    })
+                    //                    .padding()
+                    
+                    NavigationLink(destination: PaymentView()
+                        .environmentObject(cartManager)) {
+                            Text("Proceed to Payment")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(height: 55)
+                                .frame(maxWidth: .infinity)
+                                .background(Color.green)
+                                .cornerRadius(10)
+                                .padding()
+                        }
+                    
+                } else {
+                    
+                        
+                        Text("Your cart is empty")
+                    }
+                    
+                
+            }
+            .navigationTitle(Text("My Cart"))
+            .padding(.top)
+            .toolbar {
+                Button(action: {
+                    signOut()
+                }) {
+                    Text("Sign Out")
+                        .foregroundColor(.red)
+                }
+            }
+            .fullScreenCover(isPresented: $showSignInView) {
+                SignInView(showAuthenticationView: .constant(true))
+            }
+        }
+    }
+    func signOut() {
+        Task {
+            do {
+                try settingViewModel.signOut()
+                showSignInView = true
+            } catch {
+                print("Failed to sign out: \(error)")
+            }
+        }
     }
 }
 
-#Preview {
-    CartView()
+struct CartView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartView()
+            .environmentObject(CartManager())
+    }
 }

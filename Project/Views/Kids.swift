c0dd9e2906dd8b4965a1423fb2f1d1543f60f0f4
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-26 22:18:18 -0400

    Kids updated

--- Project/Views/Kids.swift
+++ Project/Views/Kids.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct Kids: View {
+    
+    @StateObject var cartManager = CartManager()
+    var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList2, id: \.id) { product in
+                        ProductCard(product: product)
+                            .environmentObject(cartManager)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(Text("Kids"))
+            .toolbar {
+                NavigationLink {
+                    CartView()
+                        .environmentObject(cartManager)
+                } label: {
+                    CartButton(numberOfProducts: cartManager.products.count)
+                }
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
-#Preview {
-    Kids()
+struct Kids_Previews: PreviewProvider {
+    static var previews: some View {
+        Kids()
+    }
 }

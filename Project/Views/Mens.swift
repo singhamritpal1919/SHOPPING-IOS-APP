cf79bac0ba577bb736770fa848b8a262c7008ae8
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-26 22:18:53 -0400

    mens updated

--- Project/Views/Mens.swift
+++ Project/Views/Mens.swift
@@ -7,12 +7,39 @@
 
 import SwiftUI
 
+
+
 struct Mens: View {
+    @StateObject var cartManager = CartManager()
+    var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList1, id: \.id) { product in
+                        ProductCard(product: product)
+                            .environmentObject(cartManager)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(Text("Mens"))
+            .toolbar {
+                NavigationLink {
+                    CartView()
+                        .environmentObject(cartManager)
+                } label: {
+                    CartButton(numberOfProducts: cartManager.products.count)
+                }
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
-#Preview {
-    Mens()
+struct Mens_Previews: PreviewProvider {
+    static var previews: some View {
+        Mens()
+    }
 }

0014979099e76b3f0d652296347522430e3a22fd
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-27 22:06:11 -0400

    womens

--- Project/Views/Womens.swift
+++ Project/Views/Womens.swift
@@ -5,14 +5,38 @@
 //  Created by Amritpal Gill on 2024-08-12.
 //
 
+
 import SwiftUI
 
 struct Womens: View {
+    @EnvironmentObject var cartManager: CartManager  // Use EnvironmentObject here
+    var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(productList, id: \.id) { product in
+                    ProductCard(product: product)
+                        .environmentObject(cartManager)
+                }
+            }
+            .padding()
+        }
+        .navigationTitle(Text("Womens"))
+        .toolbar {
+            NavigationLink {
+                CartView()
+                    .environmentObject(cartManager)
+            } label: {
+                CartButton(numberOfProducts: cartManager.products.count)
+            }
+        }
     }
 }
 
-#Preview {
-    Womens()
+struct Womens_Previews: PreviewProvider {
+    static var previews: some View {
+        Womens()
+            .environmentObject(CartManager())  // Provide a CartManager instance for previews
+    }
 }

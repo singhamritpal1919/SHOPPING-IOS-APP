affe06fcb1f5a949a6cd2c0a19c8c65de844c95a
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-27 22:05:14 -0400

    Sweater

--- Project/Views/Sweater.swift
+++ Project/Views/Sweater.swift
@@ -7,12 +7,39 @@
 
 import SwiftUI
 
+
+
 struct Sweater: View {
+    @StateObject var cartManager = CartManager()
+    var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList, id: \.id) { product in
+                        ProductCard(product: product)
+                            .environmentObject(cartManager)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(Text("Womens"))
+            .toolbar {
+                NavigationLink {
+                    CartView()
+                        .environmentObject(cartManager)
+                } label: {
+                    CartButton(numberOfProducts: cartManager.products.count)
+                }
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
-#Preview {
-    Sweater()
+struct Sweater_Previews: PreviewProvider {
+    static var previews: some View {
+        Sweater()
+    }
 }

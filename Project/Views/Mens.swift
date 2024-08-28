18e63d409da238a640b7210890624718b749ae85
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-27 22:05:49 -0400

    mens

--- Project/Views/Mens.swift
+++ Project/Views/Mens.swift
@@ -5,41 +5,38 @@
 //  Created by Amritpal Gill on 2024-07-09.
 //
 
+
 import SwiftUI
 
-
-
 struct Mens: View {
-    @StateObject var cartManager = CartManager()
+    @EnvironmentObject var cartManager: CartManager  // Use EnvironmentObject here
     var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
-    
+
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(productList1, id: \.id) { product in
-                        ProductCard(product: product)
-                            .environmentObject(cartManager)
-                    }
-                }
-                .padding()
-            }
-            .navigationTitle(Text("Mens"))
-            .toolbar {
-                NavigationLink {
-                    CartView()
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(productList1, id: \.id) { product in
+                    ProductCard(product: product)
                         .environmentObject(cartManager)
-                } label: {
-                    CartButton(numberOfProducts: cartManager.products.count)
                 }
+            }
+            .padding()
+        }
+        .navigationTitle(Text("Mens"))
+        .toolbar {
+            NavigationLink {
+                CartView()
+                    .environmentObject(cartManager)
+            } label: {
+                CartButton(numberOfProducts: cartManager.products.count)
             }
         }
-        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
 struct Mens_Previews: PreviewProvider {
     static var previews: some View {
         Mens()
+            .environmentObject(CartManager())  // Provide a CartManager instance for previews
     }
 }

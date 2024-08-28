37edc2dce1a04c2ec35e32c541a17ca75939ef19
Your Github Username <your-singhamritpal1919@gmail.com>
2024-08-27 22:03:30 -0400

    kids

--- Project/Views/Kids.swift
+++ Project/Views/Kids.swift
@@ -5,40 +5,40 @@
 //  Created by Amritpal Gill on 2024-08-05.
 //
 
+
+
+
 import SwiftUI
 
 struct Kids: View {
-    
-    @StateObject var cartManager = CartManager()
+    @EnvironmentObject var cartManager: CartManager  // Use EnvironmentObject here
     var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
-    
+
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(productList2, id: \.id) { product in
-                        ProductCard(product: product)
-                            .environmentObject(cartManager)
-                    }
-                }
-                .padding()
-            }
-            .navigationTitle(Text("Kids"))
-            .toolbar {
-                NavigationLink {
-                    CartView()
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(productList2, id: \.id) { product in
+                    ProductCard(product: product)
                         .environmentObject(cartManager)
-                } label: {
-                    CartButton(numberOfProducts: cartManager.products.count)
                 }
+            }
+            .padding()
+        }
+        .navigationTitle(Text("Kids"))
+        .toolbar {
+            NavigationLink {
+                CartView()
+                    .environmentObject(cartManager)
+            } label: {
+                CartButton(numberOfProducts: cartManager.products.count)
             }
         }
-        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
 struct Kids_Previews: PreviewProvider {
     static var previews: some View {
         Kids()
+            .environmentObject(CartManager())  // Provide a CartManager instance for previews
     }
 }
